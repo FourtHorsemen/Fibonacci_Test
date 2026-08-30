d0fb58ab82a45a80a6fc1c8515e6ca88d08a0e5a
FourtHorsemen <the.fourth.horsemen4@gmail.com>
2026-08-30 20:45:14 +0300

    FibanacciSecondWay

---
+++ Fibonacci_sequence2.py
@@ -0,0 +1,9 @@
+n = int(input("Enter value: "))
+
+fibonacci = [0, 1]
+
+for i in range(2, n):
+    next_number = fibonacci[i - 1] + fibonacci[i - 2]
+    fibonacci.append(next_number)
+
+print(fibonacci)
